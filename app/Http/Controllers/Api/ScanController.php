cc26f8b30b9656ead3d4e8aaaa77a7427fbfff65
vsollery <vsollery@gmail.com>
2025-06-16 09:13:43 +0200

    add query to filter fraud or not

--- app/Http/Controllers/Api/ScanController.php
+++ app/Http/Controllers/Api/ScanController.php
@@ -13,11 +13,25 @@ class ScanController extends Controller
         return response()->json($scans, 200);
     }
 
-    public function show($id){
+    public function show(Request $request, $id){
         $scan = Scan::with('customers')->find($id);
+
         if(!$scan){
             return response()->json(['message' => 'Scan not found'], 404);
         }
-        return response()->json($scan, 200);
+        $fraud = $request->query('fraud'); // 'true', 'false', or null
+        $customers = $scan->customers;
+
+        if ($fraud === 'true') {
+            $customers = $customers->filter(fn($customer) => $customer->pivot->is_fraudulent == 1);
+        } elseif ($fraud === 'false') {
+            $customers = $customers->filter(fn($customer) => $customer->pivot->is_fraudulent == 0);
+        }
+
+        return response()->json([
+            'scan_id' =>$scan->id,
+            'scan_date' => $scan->scan_date,
+            'customers' => $customers->values(),
+        ], 200);
     }
 }
